450b0b10455fc83dbb10c575d8a104ade3c59231
victorsmart <victchubxy@gmail.com>
2022-06-16 21:36:46 -0700

    first commit

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,6 @@ if(theme == null){
 
 let themeDots = document.getElementsByClassName('theme-dot')
 
-
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
@@ -21,19 +20,19 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = "{% static 'css/default.css' %}"
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = "{% static 'css/blue.css' %}"
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById('theme-style').href = "{% static 'css/green.css' %}"
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById('theme-style').href = "{% static 'css/purple.css' %}"
 	}
 
 	localStorage.setItem('theme', mode)
